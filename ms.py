9fd15e01ed02a253ba50cc2a5525996f599d955b
root <root@LAPTOP-3DPAOQGL.localdomain>
2022-02-13 19:57:12 -0500

    wiki link added

--- ms.py
+++ ms.py
@@ -13,7 +13,7 @@ def index():
     api_key = api_key.splitlines(0)[0] ##gets rid of line break
     file.close()
 
-    movie_list = ["9323", "550", "115"]
+    movie_list = ["238", "550", "115"]
 
     movie_id = random.randrange(3)
 
@@ -22,23 +22,34 @@ def index():
     response = urllib.request.urlopen(trending_url)
     data = response.read()
     text = data.decode('utf-8')
-
     movie_json = json.loads(text)
 
     Title = movie_json['original_title']
-    Overview = movie_json['overview']
+    Overview = movie_json['tagline']
     Poster_path = movie_json['poster_path']
-
+   
+    wiki_api_url = "https://en.wikipedia.org/w/api.php?action=query&generator=links&gpllimit=500&format=json&titles="+Title.lower().replace(" ", "_")+"&prop=info&inprop=url"
+    
+    response = urllib.request.urlopen(wiki_api_url)
+    data = response.read()
+    text = data.decode('utf-8')
+    wiki_json = json.loads(text)
+    wiki_url = ""
+    values = wiki_json['query']['pages'].values()
+   
+    for i in values:
+    	if Title in i['title']:
+    		wiki_url = i['fullurl']
+    		break;
+   
     poster_url = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2"+Poster_path
-
-
 
     genres = ""
 
     for i in movie_json['genres']:
-        genres = genres +i['name']+","
+        genres = genres +i['name']+", "
         
-    return flask.render_template("index.html", title = Title, overview = Overview, genre = genres, poster = poster_url )
+    return flask.render_template("index.html", title = Title, overview = Overview, genre = genres, poster = poster_url, wiki = wiki_url )
 app.run(
      debug=True
 )
