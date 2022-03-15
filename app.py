177615cf19caab4b0df2f14ba0ceed8b0e7a4272
root <root@LAPTOP-3DPAOQGL.localdomain>
2022-03-15 10:48:24 -0400

    final commit

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ bp = flask.Blueprint(
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://zqqntapzbixydu:32206c2e30f69fa017327dc53c27798f78f4dbef2804b19bbda5004d4aa1ed88@ec2-3-212-45-192.compute-1.amazonaws.com:5432/devsdleosdarcp"
 
 
 db = SQLAlchemy(app)
