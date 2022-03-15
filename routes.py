546768581b90b253704fecd02a71c7131903dd3a
root <root@LAPTOP-3DPAOQGL.localdomain>
2022-03-15 10:12:36 -0400

    final commit

--- routes.py
+++ routes.py
@@ -176,6 +176,5 @@ def fetchMovie(user="", id=-1):
     )
 
 
-app.run(
-    host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
-)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
